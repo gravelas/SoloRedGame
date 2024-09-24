af52ec7d5ab36f44de4df137c66fe0db45b30728
Tyler Westerberg <tylerwesterberg05@gmail.com>
2024-09-24 16:33:11 -0400

    Added RedCard canvas rule

--- src/cs3500/solored/model/hw02/RedCard.java
+++ src/cs3500/solored/model/hw02/RedCard.java
@@ -1,5 +1,8 @@
 package cs3500.solored.model.hw02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RedCard extends SoloCard{
 
   /**
@@ -10,4 +13,28 @@ public class RedCard extends SoloCard{
     super(num);
     this.color = Color.RED;
   }
+
+  @Override
+  public List<Boolean> canvasRule(List<List<SoloCard>> palettes) {
+    int largestPaletteIndex = 0;
+    int largestPaletteNumber = -1;
+    for (int paletteIndex = 1; paletteIndex < palettes.size(); paletteIndex++) {
+        int largestCardNum = palettes.get(paletteIndex).get(0).number();
+        for (int cardIndex = 1; cardIndex < palettes.get(paletteIndex).size(); cardIndex++) {
+          if (palettes.get(paletteIndex).get(cardIndex).number() > largestCardNum) {
+            largestCardNum = palettes.get(paletteIndex).get(cardIndex).number();
+          }
+        }
+        if (largestCardNum > largestPaletteNumber) {
+          largestPaletteIndex = paletteIndex;
+          largestPaletteNumber = largestCardNum;
+        }
+      }
+    List<Boolean> winningPaletteList = new ArrayList<>();
+    for (int paletteIndex = 0; paletteIndex < palettes.size(); paletteIndex++) {
+      winningPaletteList.add(paletteIndex == largestPaletteIndex);
+    }
+    return winningPaletteList;
+  }
+
 }
