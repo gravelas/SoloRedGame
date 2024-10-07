ca95b1223d2d7461e2efcd10809d63d1be3042ef
Tyler Westerberg <tylerwesterberg05@gmail.com>
2024-10-07 11:05:00 -0400

    moved files

---
+++ src/cs3500/solored/controller/RedGameController.java
@@ -0,0 +1,11 @@
+package cs3500.controller;
+
+import java.util.List;
+
+import cs3500.solored.model.hw02.Card;
+import cs3500.solored.model.hw02.RedGameModel;
+
+public interface RedGameController<C extends Card> {
+
+  void playGame(RedGameModel<C> model, List<C> deck, boolean shuffle, int numPalettes, int handSize) throws IllegalArgumentException, IllegalStateException;
+}
