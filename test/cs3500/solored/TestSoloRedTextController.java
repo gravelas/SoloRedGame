3fb006e1e17ee805b27aed9eecf685fce8f57029
Tyler Westerberg <tylerwesterberg05@gmail.com>
2024-10-08 18:10:16 -0400

    added tests plus a mock

--- test/cs3500/solored/TestSoloRedTextController.java
+++ test/cs3500/solored/TestSoloRedTextController.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.util.List;
 
 import cs3500.solored.controller.RedGameController;
 import cs3500.solored.controller.SoloRedTextController;
@@ -144,6 +145,173 @@ public class TestSoloRedTextController {
 
   @Test
   public void testInvalidCommandWithSomeValid() throws IOException {
-    Assert.assertTrue(true);
+    Readable rd = new StringReader("canvas 1 lkj q");
+    Appendable ap = new StringBuilder();
+    ap.append("Canvas: R\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "P3: V3\n" +
+            "> P4: V4\n" +
+            "Hand: V5 V6 V7 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Canvas: V\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "> P3: V3\n" +
+            "P4: V4\n" +
+            "Hand: V6 V7 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Invalid command. Try again. Enter palette, canvas, or q/Q.\n" +
+            "Canvas: V\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "> P3: V3\n" +
+            "P4: V4\n" +
+            "Hand: V6 V7 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Game quit!\n" +
+            "State of game when quit:\n" +
+            "Canvas: V\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "> P3: V3\n" +
+            "P4: V4\n" +
+            "Hand: V6 V7 I1\n" +
+            "Number of cards in deck: 2\n");
+
+    Appendable ap2 = new StringBuilder();
+    RedGameController controller = new SoloRedTextController(rd, ap2);
+
+    RedGameModel<SoloCard> model = new SoloRedGameModel();
+
+    controller.playGame(model, model.getAllCards().subList(0,10), false, 4, 4);
+
+    Assert.assertEquals(ap2.toString(), ap.toString());
   }
+
+  @Test
+  public void testModelAlreadyStartedThrowsIllegalArgumentException() throws IOException {
+    Readable rd = new StringReader("canvas 1 lkj q");
+    Appendable ap2 = new StringBuilder();
+    RedGameController controller = new SoloRedTextController(rd, ap2);
+
+    RedGameModel<SoloCard> model = new SoloRedGameModel();
+    model.startGame(model.getAllCards().subList(0,10), false, 4, 4);
+
+    Assert.assertThrows(
+            IllegalArgumentException.class, () -> controller.playGame(
+                    model, model.getAllCards().subList(0,10), false, 4, 4
+            )
+    );
+  }
+
+  @Test
+  public void testModelIsNullThrowsIllegalArgumentException() throws IOException {
+    Readable rd = new StringReader("canvas 1 lkj q");
+    Appendable ap2 = new StringBuilder();
+    RedGameController controller = new SoloRedTextController(rd, ap2);
+
+    RedGameModel<SoloCard> model = null;
+
+    Assert.assertThrows(
+            IllegalArgumentException.class, () -> controller.playGame(
+                    model, List.of(), false, 4, 4
+            )
+    );
+  }
+
+  @Test
+  public void testReadableNullThrowsIllegalArgumentException() throws IOException {
+    Readable rd = null;
+    Appendable ap2 = new StringBuilder();
+    Assert.assertThrows(IllegalArgumentException.class, () -> new SoloRedTextController(rd, ap2));
+  }
+
+  @Test
+  public void testAppendableIOExceptionThrowsIllegalStateException() {
+    Readable rd = new StringReader("canvas 1 lkj q");
+    Appendable ap2 = new ThrowingAppendableMock();
+    RedGameController controller = new SoloRedTextController(rd, ap2);
+
+    RedGameModel<SoloCard> model = new SoloRedGameModel();
+
+    Assert.assertThrows(
+            IllegalStateException.class, () -> controller.playGame(
+                    model, model.getAllCards().subList(0,10), false, 4, 4
+            )
+    );
+  }
+
+  @Test
+  public void testNegativeNumberAndLetterFiltering() {
+    Readable rd = new StringReader("canvas -1 w e r t y 2 palette -2 -3 -4 4 2 q");
+    Appendable ap2 = new StringBuilder();
+    Appendable ap = new StringBuilder("Canvas: R\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "P3: V3\n" +
+            "> P4: V4\n" +
+            "Hand: V5 V6 V7 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Canvas: V\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "> P3: V3\n" +
+            "P4: V4\n" +
+            "Hand: V5 V7 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Canvas: V\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "> P3: V3\n" +
+            "P4: V4 V7\n" +
+            "Hand: V5 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Game lost.\n" +
+            "Canvas: V\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "> P3: V3\n" +
+            "P4: V4 V7\n" +
+            "Hand: V5 I1\n" +
+            "Number of cards in deck: 2\n");
+    RedGameController controller = new SoloRedTextController(rd, ap2);
+
+    RedGameModel<SoloCard> model = new SoloRedGameModel();
+
+    controller.playGame(model, model.getAllCards().subList(0,10), false, 4, 4);
+
+    Assert.assertEquals(ap.toString(), ap2.toString());
+  }
+
+  @Test
+  public void testQuittingMidCommand() {
+    Readable rd = new StringReader("canvas q 1");
+    Appendable ap2 = new StringBuilder("Canvas: R\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "P3: V3\n" +
+            "> P4: V4\n" +
+            "Hand: V5 V6 V7 I1\n" +
+            "Number of cards in deck: 2\n" +
+            "Game quit!\n" +
+            "State of game when quit:\n" +
+            "Canvas: R\n" +
+            "P1: V1\n" +
+            "P2: V2\n" +
+            "P3: V3\n" +
+            "> P4: V4\n" +
+            "Hand: V5 V6 V7 I1\n" +
+            "Number of cards in deck: 2\n");
+    Appendable ap = new StringBuilder();
+    RedGameController controller = new SoloRedTextController(rd, ap);
+
+    RedGameModel<SoloCard> model = new SoloRedGameModel();
+
+    controller.playGame(model, model.getAllCards().subList(0,10), false, 4, 4);
+
+    Assert.assertEquals(ap2.toString(), ap.toString());
+  }
+
+
 }
---
+++ test/cs3500/solored/ThrowingAppendableMock.java
@@ -0,0 +1,20 @@
+package cs3500.solored;
+
+import java.io.IOException;
+
+public class ThrowingAppendableMock implements Appendable {
+  @Override
+  public Appendable append(CharSequence csq) throws IOException {
+    throw new IOException();
+  }
+
+  @Override
+  public Appendable append(CharSequence csq, int start, int end) throws IOException {
+    return null;
+  }
+
+  @Override
+  public Appendable append(char c) throws IOException {
+    throw new IOException();
+  }
+}
