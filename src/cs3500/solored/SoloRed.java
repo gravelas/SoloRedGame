b01465b2e66ef964b47b6b738932b72a95cf52ce
Tyler Westerberg <tylerwesterberg05@gmail.com>
2024-10-17 12:27:14 -0400

    more comments to fix style issues

--- src/cs3500/solored/SoloRed.java
+++ src/cs3500/solored/SoloRed.java
@@ -11,11 +11,19 @@ import cs3500.solored.model.hw02.RedGameModel;
 import cs3500.solored.model.hw02.SoloCard;
 import cs3500.solored.model.hw04.RedGameCreator;
 
+/**
+ * Main runner class for the SoloRed Game. Creates a View, Controller, and Model to manage
+ * and run the game with user input/output.
+ */
 public class SoloRed {
 
   private static final int defaultHandSize = 7;
   private static final int defaultPalettesSize = 4;
 
+  /**
+   * Main class that runs the program in the console window.
+   * @param args type of game, palettesSize, handSize
+   */
   public static void main(String[] args) {
     if (args.length == 0) {
       throw new IllegalArgumentException("no arguments passed.");
@@ -48,7 +56,13 @@ public class SoloRed {
 
   }
 
-  public static int parsePaletteInt(String input) {
+  /**
+   * Takes a string input and checks if an integer or less than one.
+   * If not in bounds it sets the return to the defaultPalettesSize.
+   * @param input string input to parse.
+   * @return int of either the input or defaultPaletteSize
+   */
+  private static int parsePaletteInt(String input) {
     int inputPaletteSize;
     try {
       inputPaletteSize = Integer.parseInt(input);
@@ -61,7 +75,13 @@ public class SoloRed {
     return inputPaletteSize;
   }
 
-  public static int parseHandInt(String input) {
+  /**
+   * Takes a string input and checks if an integer or less than one.
+   * If not in bounds it sets the return to the defaultHandSize.
+   * @param input string input to parse.
+   * @return int of either the input or defaultHandSize
+   */
+  private static int parseHandInt(String input) {
     int inputHandSize;
     try {
       inputHandSize = Integer.parseInt(input);
